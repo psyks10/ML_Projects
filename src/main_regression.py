dee75f69a8ddadc5484d5400117683cb44fa3f05
kimsagha <psyks10@nottingham.ac.uk>
2020-11-06 18:00:23 +0000

    skeleton

--- src/main_regression.py
+++ src/main_regression.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env
 
-import functions.logger as logger
 import numpy as np
 import tensorflow as tf
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+
+from src import logger
 
 
 class TensorFlow():
