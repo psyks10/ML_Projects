695ed8dabc1fc3af277b21eb50bad475670e1e34
psyjt4 <psyjt4@nottingham.ac.uk>
2020-11-13 18:51:57 +0000

    new kfold

--- src/main_regression.py
+++ src/main_regression.py
@@ -2,9 +2,7 @@ import numpy as np
 import tensorflow as tf
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
 from src.logger import initialise_logger
 
 
@@ -46,7 +44,7 @@ class TensorFlow:
         n_hidden2 = 10
         n_hidden3 = 10
         n_input = 16
-        n_output = 7
+        n_output = 1
 
         # Learning parameters
         learning_constant = 0.3
@@ -88,76 +86,73 @@ class TensorFlow:
         init = tf.global_variables_initializer()
 
         # Load the data
-        X_data, y_data, X_train, X_test, X_val, y_train, y_test, y_val, label = self.load_obesity_data()
+        X_data, y_data, X_train, X_test, y_train, y_test = self.load_obesity_data()
 
         with tf.Session() as sess:
 
             sess.run(init)
 
-            for epoch in range(number_epochs):
+            test_data_accuracies = []
 
-                avg_cost = 0.0
-                total_batch = int(len(X_train) / batch_size)
-                X_train_batches = np.array_split(X_train, total_batch)
-                y_train_batches = np.array_split(y_train, total_batch)
+            # K-Fold
+            k_fold = KFold(n_splits=10, random_state=self.seed)
+            i = 0
+            for train_indices, test_indices in k_fold.split(X_train):
+                i += 1
+                print(f"\nFold no. {i}")
 
-                # Mini-batch training
-                for X_batch, y_batch in zip(X_train_batches, y_train_batches):
-                     sess.run(optimizer, feed_dict={X: X_batch, Y: y_batch})
+                X_train_fold, y_train_fold = X_train[train_indices], y_train[train_indices]
+                X_test_fold, y_test_fold = X_train[test_indices], y_train[test_indices]
+                for epoch in range(number_epochs):
+                    _, c = sess.run([optimizer, loss_op], feed_dict={X: X_train_fold, Y: y_train_fold})
 
-                # Display the epoch
-                if epoch % (number_epochs/10) == 0:
-                    print("Epoch:", '%d' % epoch)
-                    pred = sess.run(neural_network, feed_dict={X: X_test})
-                    #print("prediction:", np.argmax(pred, axis=1))
-                    #print("y predicts:", np.argmax(y_test, axis=1))
-                    print("Cost:", sess.run(loss_op, feed_dict={X: X_test, Y: y_test}))
-                    accuracy = tf.keras.losses.MSE(pred, Y)
-                    print("Accuracy:", sess.run(accuracy, feed_dict={X: X_test, Y: y_test}))
-                    print("______________________________________")
+                # Evaluation
+                pred = (neural_network)  # Apply softmax to logits
+                estimated_class = tf.argmax(pred, axis=1)
 
+                correct_prediction1 = tf.equal(tf.argmax(pred, axis=1), pd.DataFrame(y_test_fold).idxmax(axis=1).values)
+                accuracy1 = tf.reduce_mean(tf.cast(correct_prediction1, tf.float32))
+                print(f'Class estimation: {sess.run(estimated_class, feed_dict={X: X_test_fold})}')
+                print(f'Correct predictions: {sess.run(correct_prediction1, feed_dict={X: X_test_fold}).sum()} '
+                      f'out of {len(y_test_fold)}')
+                print(f'Accuracy: {accuracy1.eval({X: X_test_fold})}')
+                test_data_accuracies.append(accuracy1.eval({X: X_test_fold}))
 
-            # Test model
-            pred = (neural_network)  # Apply softmax to logits
-            accuracy = tf.keras.losses.MSE(pred, Y)
-            print("Accuracy:", accuracy.eval({X: X_train, Y: y_train}))
-            print("Prediction:", pred.eval({X: X_train}))
-            output = neural_network.eval({X: X_train})
+                print(f"Cost: {c}")
 
-            plt.plot(y_train[0:10], 'ro', output[0:10], 'bo')
-            plt.ylabel('some numbers')
-            plt.show()
+            # Evaluation - training data
+            print(f'K-Fold accuracy: {test_data_accuracies}')
+            print(f'Average K-Fold accuracy: {np.mean(test_data_accuracies)}')
 
-            # Training accuracy
-            estimated_class = tf.argmax(pred, 1)  # +1e-50-1e-50
-            correct_prediction1 = tf.equal(estimated_class, label)
-            accuracy1 = tf.reduce_mean(tf.cast(correct_prediction1, tf.float32))
-            #print("correct_prediction1: ", correct_prediction1)
-            #print("estimated_class: ", estimated_class)
-            print(accuracy1.eval({X: X_data}))
-
-            # Validation accuracy
+            # Evaluation - test data
             estimated_class = tf.argmax(pred, axis=1)  # +1e-50-1e-50
-            correct_prediction1 = tf.equal(estimated_class, pd.DataFrame(y_val).idxmax(axis=1).values)
+            correct_prediction1 = tf.equal(tf.argmax(pred, axis=1), pd.DataFrame(y_test).idxmax(axis=1).values)
             accuracy1 = tf.reduce_mean(tf.cast(correct_prediction1, tf.float32))
-            #print("correct_prediction1: ", correct_prediction1)
-            #print("estimated_class: ", estimated_class)
-            print("accuracy1", accuracy1.eval({X: X_val}))
 
+            print('\nEvaluation of test data')
+            print(f'Class estimation: {sess.run(estimated_class, feed_dict={X: X_test})}')
+            print(f'Correct predictions: {sess.run(correct_prediction1, feed_dict={X: X_test}).sum()} '
+                  f'out of {len(X_test)}')
+            print(f'Accuracy: {accuracy1.eval({X: X_test})}')
 
-
-
-
+    def generate_summary(self, X_placeholder, Y_placeholder, X_values, y_values, accuracy, neural_network, session):
+        result = pd.DataFrame()
+        result['label'] = pd.DataFrame(y_values).idxmax(axis=1).values
+        result['label_encoded'] = list(y_values)
+        result['output'] = list(neural_network.eval({X_placeholder: X_values}))
+        result['prediction'] = list(
+            session.run(tf.argmax(neural_network, axis=1), feed_dict={X_placeholder: X_values}))
+        result['accuracy'] = list(accuracy.eval({X_placeholder: X_values, Y_placeholder: y_values}))
+        return result
 
     def load_obesity_data(self):
         # Separate features and label
         y_data = pd.DataFrame(self.data['NObeyesdad'])
         X_data = self.data.drop('NObeyesdad', axis=1)
-        # Set label
-        label = self.data['NObeyesdad'].astype('category').cat.codes
-        label = label.values
+
         # Encode the label using dummies
-        y_data = pd.get_dummies(y_data)
+        #y_data = pd.get_dummies(y_data)
+        y_data = pd.DataFrame(y_data['NObeyesdad'].astype('category').cat.codes)
 
         # Encoding categorical features
         categorical_columns = ["Gender", "family_history_with_overweight", "FAVC", "CAEC", "SMOKE", "SCC", "CALC",
@@ -169,14 +164,13 @@ class TensorFlow:
         X_data = ((X_data - X_data.min()) / (X_data.max() - X_data.min()))
         y_data = ((y_data - y_data.min()) / (y_data.max() - y_data.min()))
 
-        # Split data in Training 60%, Test 20% and Validation 20%
-        X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.40, random_state=self.seed)
-        X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.50, random_state=self.seed)
+        # Split data in Training 80%, Test 20%
+        X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2, random_state=self.seed)
 
         # Convert to arrays
-        X_train, X_test, X_val, y_train, y_test, y_val = X_train.values, X_test.values, X_val.values, y_train.values, \
-                y_test.values, y_val.values
-        return X_data, y_data, X_train, X_test, X_val, y_train, y_test, y_val, label
+        X_train, X_test, y_train, y_test, = X_train.values, X_test.values, y_train.values, y_test.values,
+
+        return X_data, y_data, X_train, X_test, y_train, y_test
 
     def multilayer_perceptron(self, input_d):
         # Task of neurons of first hidden layer
