fd1f97a4c7b82a4f109505cc7b81ef78f671296d
Alessia <psyao3@nottingham.ac.uk>
2020-11-28 12:54:27 +0000

    Added fix to shape of kids and -1 bestAttribute

--- DecisionTrees/calculateEntropy.m
+++ DecisionTrees/calculateEntropy.m
@@ -1,4 +1,8 @@
 function entropy = calculateEntropy(positiveRatio, negativeRatio)
     % high entropy = low purity 
-    entropy = - positiveRatio * log2(positiveRatio) - negativeRatio * log2(negativeRatio);
+    if positiveRatio == 1 || negativeRatio == 1
+        entropy = 0;
+    else
+        entropy = - positiveRatio * log2(positiveRatio) - negativeRatio * log2(negativeRatio);
+    end
 end
