fa640a85688385e54a76c97890cec23ba989b147
psyjt4 <psyjt4@nottingham.ac.uk>
2020-12-01 00:12:01 +0000

    table splitter

--- DecisionTrees/splittables.m
+++ DecisionTrees/splittables.m
@@ -1,4 +1,5 @@
-table = % smthing here
-edges=[1:ceil(height(table)/10):height(table),height(table)]
+featurs,labels = loadDataset(1);
+
+edges=[1:ceil(height(table)/10):height(table),height(table)];
 discretize(1:height(table), edges).'
-splitTables = splitapply(@(x) {cell2table(x)}, table2cell(table), discretize(1:height(table), edges).');
+splitTables = splitapply(@(x) {cell2table(x, 'VariableNames', table.Properties.VariableNames)}, table2cell(table), discretize(1:height(table), edges).');
