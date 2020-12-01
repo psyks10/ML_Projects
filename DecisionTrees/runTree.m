e7a3bd8a1b0f9ee018ead1f815620059943180a7
Alessia <psyao3@nottingham.ac.uk>
2020-12-01 12:06:37 +0000

    Added runTree.m

---
+++ DecisionTrees/runTree.m
@@ -0,0 +1,17 @@
+function treePrediction = runTree(features, tree)
+    treePrediction = cell(height(features),1);
+    
+    for index = 1:height(features)
+        decisionTree = tree;
+        while ~isempty(decisionTree.kids)
+            iAttributeValue = string(features.(decisionTree.op){index});
+            iThreshold = string(decisionTree.threshold);
+            if strcmp(iAttributeValue, iThreshold)
+               decisionTree = decisionTree.kids{1};
+            else
+                decisionTree = decisionTree.kids{2};
+            end
+        end
+        treePrediction{index,1} = decisionTree.class;
+    end
+end
