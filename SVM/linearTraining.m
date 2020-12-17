53bb29e072e8b20f7181a2a22b20a86556a22af5
psyjt4 <psyjt4@nottingham.ac.uk>
2020-12-17 10:57:39 +0000

    final linear training

--- SVM/linearTraining.m
+++ SVM/linearTraining.m
@@ -1,36 +1,49 @@
 function results = linearTraining(type, features, labels)
 
     if type
-        results = struct('Error', NaN,'NoOfSupportVectors', NaN);
+        results = struct('NoOfSupportVectors', NaN, 'Metrics', NaN, 'labels', NaN);
     else
-        results = struct('Epsilon', NaN, 'Error', NaN,'NoOfSupportVectors', NaN);
+        results = struct('Epsilon', NaN, 'RMSE', NaN,'NoOfSupportVectors', NaN, 'labels', NaN);
     end
     
-    trainIdx = 1:ceil(height(features));
+    trainIdx = 1:ceil(height(features)*0.8);
     testIdx =  setdiff(1:height(features),trainIdx);
 
     trainData = features(trainIdx, :);
-    trainTarg = labels(trainIdx,:);
+    trainLabels = labels(trainIdx,:);
 
     testData = features(testIdx, :);
-    testTarg = labels(testIdx,:);
+    testLabels = labels(testIdx,:);
     
     if type
         epsilons = 1:1;
     else
-        epsilons = [0.1,0.2,0.5,1];
+        epsilons = {0.1,0.2,0.5,1};
     end
     
     for i = 1:length(epsilons)
         if type
-            model = fitcsvm(trainData,trainTarg, 'KernelFunction','linear', 'BoxConstraint',1);
+            model = fitcsvm(trainData,trainLabels, 'KernelFunction','linear', 'BoxConstraint',1);
+            testPred = predict(model,testData);
+        
+            confusionMatrix = calculateConfusionMatrix(testLabels, testPred);
+            testMetrics = struct('recall',NaN,'precision',NaN,'F1Score',NaN);
+            testMetrics.recall = confusionMatrix.TP / (confusionMatrix.TP+confusionMatrix.FN);
+            testMetrics.precision = confusionMatrix.TP / (confusionMatrix.TP+confusionMatrix.FP);
+            testMetrics.F1Score = 2 * ( (testMetrics.precision*testMetrics.recall) / (testMetrics.precision+testMetrics.recall)  );
+            results(i).Metrics = testMetrics;
         else
-            model = fitrsvm(trainData,trainTarg, 'KernelFunction','linear', 'BoxConstraint',1,'Epsilon', epsilons{i});
+            model = fitrsvm(trainData,trainLabels, 'KernelFunction','linear', 'BoxConstraint',1,'Epsilon', epsilons{i});
+            testPred = predict(model,testData);
             results(i).Epsilon = epsilons{i};
+            results(i).RMSE = calculateRMSE(testLabels, testPred);
         end
-        results(i).Error = loss(model,testData, testTarg);
+        
         noOfSupportVectors =  height(model.SupportVectors);
         results(i).NoOfSupportVectors = noOfSupportVectors;
         results(i).PercentageOfSupportVectors = noOfSupportVectors/height(trainData);
+        results(i).labels = testPred.';
     end
+    
+    
 end
