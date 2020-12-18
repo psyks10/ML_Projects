685e557d2fe09fb52e86c1c6b3db6fee411d7ad3
psyjt4 <psyjt4@nottingham.ac.uk>
2020-12-18 09:51:16 +0000

    fin

--- SVM/calculateRMSE.m
+++ SVM/calculateRMSE.m
@@ -1,6 +1,9 @@
 function RMSE = calculateRMSE(actual, predicted)
 
     error = actual - predicted;
+    idx = isnan(error);
+    [delRows,~] = find(idx);
+    error(delRows,:) = [];
     squaredError = error.^2;
     meanSquaredError = mean(squaredError);
     RMSE = sqrt(meanSquaredError);
